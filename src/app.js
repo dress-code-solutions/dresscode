ba9d17d284b967e5c7f64cb8947deba2b8794042
Matt <dresscodesolutions@gmail.com>
2017-07-23 19:00:30 -0400

    start
    start

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 // http://appdividend.com/2017/06/18/node-js-express-tutorial/
+// app.js
+
+var express = require('express');
+
+var app = express();
+var port = 3000;
+
+var itemRouter = require('./src/routes/itemRoutes');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use('/items', itemRouter);
+
+app.listen(port, function () {
+  console.log('Server is running on port:', port);
+});
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
